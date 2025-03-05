5313445846a5572af6b1b33b7111e372b809e43d
Atharva262005 <atharvaralegankar2005@gmail.com>
2025-03-06 04:18:40 +0530

    connection

--- frontend/src/components/PDFReader.jsx
+++ frontend/src/components/PDFReader.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react';
+import React from 'react';
+import { useEffect, useState } from 'react';
 import { createWorker } from 'tesseract.js';
 
 export const PDFReader = ({ onTextExtracted }) => {
